33e10dcee706b741a715e7a831116e2dfe34133b
Christopher Jason <cjstudios@christohersmini.lan>
2023-02-01 22:38:48 -0500

    modified step def and feature file

--- src/test/java/com/chroma/features/Template.feature
+++ src/test/java/com/chroma/features/Template.feature
@@ -1,21 +1,10 @@
 @tag
-Feature: Title of your feature
-  I want to use this template for my feature file
+Feature: Student Information Module
 
-  @tag1
-  Scenario: Title of your scenario
-    Given I want to write a step with precondition
-    And some other precondition
-    When I complete action
-    And some other action
-    And yet another action
-    Then I validate the outcomes
-    And check more outcomes
-
-  @studentinformation
+@studentinformation
 Scenario: Inspection Of Student Information Module Links
 Given a user is on the login page "https://mexil.it/chroma_tech_academy/test/site/login"
 When user logs in with valid credentials username "general@teacher.com" and password "123456"
-And user clicks the student information dropdown 
+And user clicks the student information dropdown on dashboard page "https://mexil.it/chroma_tech_academy/test/admin/admin/dashboard"
 Then The following modules should be displayed "Student Details", "Student Admission", "Disabled Students", "Bulk Delete", "Student Catagories", "Student House", "Disable Reason"
 
--- src/test/java/com/chroma/stepDefinitions/SampleStepDefinitions.java
+++ src/test/java/com/chroma/stepDefinitions/SampleStepDefinitions.java
@@ -3,6 +3,7 @@ package com.chroma.stepDefinitions;
 import com.chroma.appsCommon.PageInitializer;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class SampleStepDefinitions extends PageInitializer{
@@ -19,6 +20,17 @@ public void a_user_is_on_the_login_page(String string) {
 @When("user logs in with valid credentials username {string} and password {string}")
 public void user_logs_in_with_valid_credentials_username_and_password(String string, String string2) {
    
+}
+@Then("user clicks the student information dropdown on dashboard page {string}")
+public void user_clicks_the_student_information_dropdown_on_dashboard_page(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new cucumber.api.PendingException();
+}
+
+@Then("The following modules should be displayed {string}, {string}, {string}, {string}, {string}, {string}, {string}")
+public void the_following_modules_should_be_displayed(String string, String string2, String string3, String string4, String string5, String string6, String string7) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new cucumber.api.PendingException();
 }
 
 }
