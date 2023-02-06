06a2f4a8aafc15f6d19542ab3133dfc4935c5bb3
Christopher Jason <cjstudios@christohersmini.lan>
2023-02-06 08:57:41 -0500

    changed feature files to REProgression

--- src/test/java/com/chroma/pages/RealEstateProjectsPage.java
+++ src/test/java/com/chroma/pages/RealEstateProjectsPage.java
@@ -10,15 +10,19 @@ public class RealEstateProjectsPage {
     /* PROJECTS PAGE ELEMENT */
     @FindBy(xpath = "//span[normalize-space()='Projects']")
     public static WebElement projectsTabPageElement;
+
     /* ADD NEW PROJECT PAGE ELEMENT */
     @FindBy(xpath = "//a[@class='btn btn-primary btn-lg sidebar-cta']")
     public static WebElement addNewProjectElement;
+
     /* PROJECTS PAGE ELEMENT */
     @FindBy(xpath = "//button[@class='sidebar-item']")
     public static WebElement projectsPageElement;
+
     /* ACTIVE RADIAL BUTTON */
     @FindBy(xpath = "//a[normalize-space()='Active']")
     public static WebElement activeRadialButton;
+    
     /* ARCHIVED RADIAL BUTTON */
     @FindBy(xpath = "//a[normalize-space()='Archived']")
     public static WebElement archivedRadialButton;
--- src/test/java/com/chroma/stepDefinitions/RealEstateProjectsStepDefinitions.java
+++ src/test/java/com/chroma/stepDefinitions/RealEstateProjectsStepDefinitions.java
@@ -3,20 +3,19 @@ package com.chroma.stepDefinitions;
 import com.chroma.pages.RealEstateProjectsPage;
 import com.chroma.utils.CucumberLogUtils;
 import com.chroma.web.CommonUtils;
-import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 
 public class RealEstateProjectsStepDefinitions {
 
-    @Given("admin clicks the projects dropdown on dashboard page {string}")
-    public void admin_clicks_the_projects_dropdown_on_dashboard_page(String string) {
-        RealEstateProjectsPage.projectsPageElement.click();
+    @Then("admin clicks the projects dropdown on dashboard page")
+    public void admin_clicks_the_projects_dropdown_on_dashboard_page() {
+        RealEstateProjectsPage.projectsTabPageElement.click();
+        CommonUtils.sleep(1000);
     }
 
     @Then("the following elements should be displayed {string}, {string}, {string}, {string}")
     public void the_following_elements_should_be_displayed(String addNewProject, String projects, String active,
             String archived) {
-
         String actualAddNewProjectText = RealEstateProjectsPage.addNewProjectElement.getText();
         String actualProjectsText = RealEstateProjectsPage.projectsPageElement.getText();
         String actualActiveText = RealEstateProjectsPage.activeRadialButton.getText();
@@ -27,6 +26,5 @@ public class RealEstateProjectsStepDefinitions {
         CommonUtils.assertEquals(actualArchivedText, archived);
         CucumberLogUtils.logScreenShot();
         CucumberLogUtils.logExtentScreenshot();
-
     }
 }
