2df7713da645f098666fd57e877268db86273fba
Geraldine Hoyos Postigo <gigi@Geraldines-MacBook-Pro.local>
2023-02-06 19:28:32 -0500

    resolved conflicts

--- src/test/java/com/chroma/appsCommon/PageInitializer.java
+++ src/test/java/com/chroma/appsCommon/PageInitializer.java
@@ -6,11 +6,8 @@ import com.chroma.pages.DashboardPage;
 import com.chroma.pages.DisableStudentPage;
 import com.chroma.pages.IncomeModulePage;
 import com.chroma.pages.LoginPage;
-<<<<<<< HEAD
 import com.chroma.pages.RealEstateAgentsPage;
-=======
 import com.chroma.pages.NewProjectPage;
->>>>>>> 8ada5f03b976a6f636b1b6f56e7acc366e71aec9
 import com.chroma.pages.RealEstateLoginPage;
 import com.chroma.pages.StudentAdmissionPage;
 import com.chroma.web.WebDriverUtils;
