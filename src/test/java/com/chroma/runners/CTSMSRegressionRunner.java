bbb5e1832cfe099706e928ed0f97b4c75659b694
Christopher Jason <cjstudios@christohersmini.lan>
2023-02-12 15:08:21 -0500

    updated runner and step def files

--- src/test/java/com/chroma/runners/CTSMSRegressionRunner.java
+++ src/test/java/com/chroma/runners/CTSMSRegressionRunner.java
@@ -11,7 +11,7 @@ import cucumber.api.junit.Cucumber;
 		features = "src/test/java/com/chroma/features", 
 		glue = "com.chroma.stepDefinitions", 
 		tags = "@CTSMSRegression",
-		dryRun = true, 
+		dryRun = false, 
 		monochrome = true, 
 		strict = true
 ) 
