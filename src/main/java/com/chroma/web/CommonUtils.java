c203d328efc1648edd911bf5ba7570905a323e5a
Юліана Кузів <kuzivkaa@gmail.com>
2023-02-12 23:13:55 -0600

    added the nonMobileScreenshots method

--- src/main/java/com/chroma/web/CommonUtils.java
+++ src/main/java/com/chroma/web/CommonUtils.java
@@ -20,6 +20,9 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
+import com.chroma.utils.ConfigReader;
+import com.chroma.utils.CucumberLogUtils;
+
 public class CommonUtils extends WebDriverUtils {
 
 	/**
@@ -505,6 +508,7 @@ public class CommonUtils extends WebDriverUtils {
 
 	/**
 	 * Use this method to assert a boolean condition
+	 * 
 	 * @param flag
 	 */
 	public static void assertTrue(boolean flag) {
@@ -512,6 +516,17 @@ public class CommonUtils extends WebDriverUtils {
 			Assert.assertTrue(flag);
 		} catch (AssertionError e) {
 			e.printStackTrace();
+		}
+	}
+
+	/*
+	 * 
+	 * Use this method to take screenshots for non mobile testing
+	 */
+	public static void nonMobileScreenshots() {
+		if (!ConfigReader.getPropertyValue("browser").equalsIgnoreCase(("mobile"))) {
+			CucumberLogUtils.logScreenShot();
+			CucumberLogUtils.logExtentScreenshot();
 		}
 	}
 }
